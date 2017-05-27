9b315d24ef5b81f84a1072a6181e7ceedccf5843
Eugene Pankov <john.pankov@gmail.com>
2017-05-27 19:01:01 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = {
+  target: 'node',
+  entry: 'src/index.ts',
+  devtool: 'source-map',
+  context: __dirname,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    pathinfo: true,
+    libraryTarget: 'umd',
+    devtoolModuleFilenameTemplate: 'webpack-terminus-clickable-links:///[resource-path]',
+  },
+  resolve: {
+    modules: ['.', 'src', 'node_modules'].map(x => path.join(__dirname, x)),
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader',
+        query: {
+          configFileName: path.resolve(__dirname, 'tsconfig.json'),
+        }
+      },
+    ]
+  },
+  externals: [
+    'fs',
+    'untildify',
+    /^rxjs/,
+    /^@angular/,
+    /^@ng-bootstrap/,
+    /^terminus-/,
+  ]
+}
